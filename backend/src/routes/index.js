c1df2ac64a6b75b0eef1dc1aef4e53273865acae
Kartikey Karanwal <kartikeykaranwal03@gmail.com>
2026-08-23 22:15:23 +0530

    Update backend/src/routes/index.js

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import scanRoutes from './scan.routes.js';
 import dashboardRoutes from './dashboard.routes.js';
+import authRoutes from './auth.routes.js';
 
 const router = Router();
 
+router.use('/auth', authRoutes);
 router.use('/scan', scanRoutes);
 router.use('/dashboard', dashboardRoutes);
 
